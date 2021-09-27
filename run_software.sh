73570c8212b041d11f8f1e46e231b379b105c479
jbwexler <jbwexler@tutanota.com>
2021-09-27 11:38:17 -0700

    convert to tabs

--- run_software.sh
+++ run_software.sh
@@ -5,130 +5,129 @@
 
 # Clone/update raw datasets and download necessary data for freesurfer/fmriprep/mriqc
 download_raw_ds () {
-  cat $ds_list | while read raw_ds || [ -n "$line" ]; do
-    raw_path="$STAGING/raw/$raw_ds"
+	cat $ds_list | while read raw_ds || [ -n "$line" ]; do
+		raw_path="$STAGING/raw/$raw_ds"
 
-    if [[ ! -d "$raw_path" ]]; then
-      datalad clone https://github.com/OpenNeuroDatasets/${raw_ds}.git "$raw_path"
+		if [[ ! -d "$raw_path" ]]; then
+			datalad clone https://github.com/OpenNeuroDatasets/${raw_ds}.git "$raw_path"
 
-      # Ensure permissions for the group
-      setfacl -R -m g:G-802037:rwX "$raw_path"
-      find "$raw_path" -type d | xargs setfacl -R -m d:g:G-802037:rwX
+			# Ensure permissions for the group
+			setfacl -R -m g:G-802037:rwX "$raw_path"
+			find "$raw_path" -type d | xargs setfacl -R -m d:g:G-802037:rwX
 	  
-	  # Get files from corral if available
-	  corral_ds="$OPENNEURO/raw/$raw_ds"
-      if [[ -d "$corral_ds" ]]; then
-		datalad update --merge -d "$corral_ds"
-	    datalad siblings configure -d "$corral_ds" -s scratch --url "$raw_path"
-		datalad push -d "$corral_ds" --to scratch --data anything
-	  fi
+			# Get files from corral if available
+			corral_ds="$OPENNEURO/raw/$raw_ds"
+			if [[ -d "$corral_ds" ]]; then
+				datalad update --merge -d "$corral_ds"
+				datalad siblings configure -d "$corral_ds" -s scratch --url "$raw_path"
+				datalad push -d "$corral_ds" --to scratch --data anything
+			fi
 
-      cd "$raw_path" 
-      find sub-*/ -regex ".*_\(T1w\|T2w\|bold\|sbref\|magnitude.*\|phase.*\|fieldmap\|epi\|FLAIR\|roi\)\.nii\(\.gz\)?" \
-            -exec datalad get {} +
-    else
-      # Update
-      cd "$raw_path"
-      datalad update --merge
-      find sub-*/ -regex ".*_\(T1w\|T2w\|bold\|sbref\|magnitude.*\|phase.*\|fieldmap\|epi\|FLAIR\|roi\)\.nii\(\.gz\)?" \
-        -exec datalad get {} +
-    fi
-  done
+			cd "$raw_path" 
+			find sub-*/ -regex ".*_\(T1w\|T2w\|bold\|sbref\|magnitude.*\|phase.*\|fieldmap\|epi\|FLAIR\|roi\)\.nii\(\.gz\)?" \
+				-exec datalad get {} +
+		else
+			# Update
+			cd "$raw_path"
+			datalad update --merge
+			find sub-*/ -regex ".*_\(T1w\|T2w\|bold\|sbref\|magnitude.*\|phase.*\|fieldmap\|epi\|FLAIR\|roi\)\.nii\(\.gz\)?" \
+				-exec datalad get {} +
+		fi
+	done
 }
 
 # Create derivatives dataset if necessary
 create_derivatives_ds () {
-  cat $ds_list | while read raw_ds || [ -n "$line" ]; do
-    derivatives_path="$STAGING/derivatives/$software/${raw_ds}-${software}"
-	raw_path="$STAGING/raw/$raw_ds"
+	cat $ds_list | while read raw_ds || [ -n "$line" ]; do
+		derivatives_path="$STAGING/derivatives/$software/${raw_ds}-${software}"
+		raw_path="$STAGING/raw/$raw_ds"
 
-    # To-do: fix reckless clones so they link to original URL; add line to download licenses; fix reckless clone issue when original dataset doesn't download properly 
-    if [[ ! -d "$derivatives_path" ]]; then
-      datalad create -c yoda "$derivatives_path"
-      cd "$derivatives_path"
-      datalad clone -d . "$STAGING/containers" code/containers --reckless ephemeral
-      git clone https://github.com/poldracklab/tacc-openneuro.git code/tacc-openneuro
-      mkdir sourcedata
-      datalad clone -d . "$raw_path" sourcedata/raw --reckless ephemeral
+		# To-do: fix reckless clones so they link to original URL; add line to download licenses; fix reckless clone issue when original dataset doesn't download properly 
+		if [[ ! -d "$derivatives_path" ]]; then
+			datalad create -c yoda "$derivatives_path"
+			cd "$derivatives_path"
+			datalad clone -d . "$STAGING/containers" code/containers --reckless ephemeral
+			git clone https://github.com/poldracklab/tacc-openneuro.git code/tacc-openneuro
+			mkdir sourcedata
+			datalad clone -d . "$raw_path" sourcedata/raw --reckless ephemeral
 	  
-	  cp code/tacc-openneuro/gitattributes_openneuro.txt .gitattributes
-	  cp code/tacc-openneuro/gitattributes_datalad_openneuro.txt .datalad/.gitattributes
+			cp code/tacc-openneuro/gitattributes_openneuro.txt .gitattributes
+			cp code/tacc-openneuro/gitattributes_datalad_openneuro.txt .datalad/.gitattributes
 
-      if [[ "$software" == "fmriprep" ]]; then
-        # Create freesurfer dataset
-		# todo: look for existing freesurfer derivatives. don't need to alter fmriprep command because it will already look in fs dir
-        fs_path="$derivatives_path/sourcedata/freesurfer"
-        datalad create -c yoda "$fs_path"
-        cd "$fs_path"
-        datalad clone -d . ///repronim/containers code/containers
-        git clone https://github.com/poldracklab/tacc-openneuro.git code/tacc-openneuro
-        mkdir sourcedata
-        datalad clone -d . https://github.com/OpenNeuroDatasets/"${raw_ds}".git sourcedata/"$raw_ds"
+			if [[ "$software" == "fmriprep" ]]; then
+				# Create freesurfer dataset
+				# todo: look for existing freesurfer derivatives. don't need to alter fmriprep command because it will already look in fs dir
+				fs_path="$derivatives_path/sourcedata/freesurfer"
+				datalad create -c yoda "$fs_path"
+				cd "$fs_path"
+				datalad clone -d . ///repronim/containers code/containers
+				git clone https://github.com/poldracklab/tacc-openneuro.git code/tacc-openneuro
+				mkdir sourcedata
+				datalad clone -d . https://github.com/OpenNeuroDatasets/"${raw_ds}".git sourcedata/"$raw_ds"
 		
-  	    cp code/tacc-openneuro/gitattributes_openneuro.txt .gitattributes
-  	    cp code/tacc-openneuro/gitattributes_datalad_openneuro.txt .datalad/.gitattributes
-      fi
+				cp code/tacc-openneuro/gitattributes_openneuro.txt .gitattributes
+				cp code/tacc-openneuro/gitattributes_datalad_openneuro.txt .datalad/.gitattributes
+			fi
 	  
-      # Ensure permissions for the group
-      setfacl -R -m g:G-802037:rwX "$derivatives_path"
-      find "$derivatives_path" -type d | xargs setfacl -R -m d:g:G-802037:rwX
-    else
-      # Update
-      datalad update --merge -d "$derivatives_path/sourcedata/raw"
-	  datalad update --merge -d "$derivatives_path/code/containers"
-	  datalad update --merge -d "$derivatives_path/code/tacc-openneuro"	  
-    fi
-  done
+			# Ensure permissions for the group
+			setfacl -R -m g:G-802037:rwX "$derivatives_path"
+			find "$derivatives_path" -type d | xargs setfacl -R -m d:g:G-802037:rwX
+		else
+			# Update
+			datalad update --merge -d "$derivatives_path/sourcedata/raw"
+			datalad update --merge -d "$derivatives_path/code/containers"
+			datalad update --merge -d "$derivatives_path/code/tacc-openneuro"	  
+		fi
+	done
 }
 
 # Run fmriprep or mriqc
 run_software () {
-  cat $ds_list | while read raw_ds || [ -n "$line" ]; do  
-    derivatives_path="$STAGING/derivatives/$software/${raw_ds}-${software}" 
-	raw_path="$STAGING/raw/$raw_ds"
-	cd "$derivatives_path"
+	cat $ds_list | while read raw_ds || [ -n "$line" ]; do  
+		derivatives_path="$STAGING/derivatives/$software/${raw_ds}-${software}" 
+		raw_path="$STAGING/raw/$raw_ds"
+		cd "$derivatives_path"
 	
-    if [[ "$software" == "fmriprep" ]]; then
-	  walltime="48:00:00"
-	  command="--output-spaces MNI152NLin2009cAsym:res-2 anat func fsaverage5 --nthreads 14 \
-	  	--omp-nthreads 7 --skip-bids-validation --notrack --fs-license-file $fs_license \
-			--use-aroma --ignore slicetiming --output-layout bids --cifti-output --resource-monitor \
-				--skull-strip-t1w $skull_strip $syn_sdc --mem_mb 38400 --bids-database-dir /tmp" 
-	elif [[ "$software" == "mriqc" ]]; then
-	  walltime="8:00:00"
-	  command="--nprocs 11 --ants-nthreads 8 --verbose-reports --dsname $raw_ds --ica --mem_gb 38"
-	  
-    fi
+		if [[ "$software" == "fmriprep" ]]; then
+			walltime="48:00:00"
+			command="--output-spaces MNI152NLin2009cAsym:res-2 anat func fsaverage5 --nthreads 14 \
+				--omp-nthreads 7 --skip-bids-validation --notrack --fs-license-file $fs_license \
+					--use-aroma --ignore slicetiming --output-layout bids --cifti-output --resource-monitor \
+						--skull-strip-t1w $skull_strip $syn_sdc --mem_mb 38400 --bids-database-dir /tmp" 
+		elif [[ "$software" == "mriqc" ]]; then
+			walltime="8:00:00"
+			command="--nprocs 11 --ants-nthreads 8 --verbose-reports --dsname $raw_ds --ica --mem_gb 38"
+		fi
 	
-	if [ -z "$all_subs_arg" ]; then
-	  all_subs=$(find "$raw_path" -maxdepth 1 -type d -name "sub-*" -printf '%f\n' | sed 's/sub-//g' | sort)
-    else
-	  all_subs=$(echo "$all_subs_arg" | sed 's/,/\n/g')
-    fi
-	
-	# Remove old work dirs
-	for sub in $all_subs; do
-      rm -rf "$work_dir/${raw_ds}_sub-$sub"
-    done
-	
-	datalad save -r
-	
-	# Submit jobs via reproman in batches 
-	# make sure to 'unlock' outputs
-	echo "$all_subs" | xargs -n "$subs_per_job" echo | while read line; do 
-	  sub_list=$(echo "$line" | sed 's/ /,/g')
-	  processes=$(echo "$line" | awk '{ print NF }')
-	  nodes=$(( ($processes + $subs_per_node - 1) / $subs_per_node)) # round up
-	  echo reproman run -r local --sub slurm --orc datalad-no-remote \
-		  --bp sub="$sub_list" --input "sourcedata/raw/sub-{p[sub]}" --output . \
-			  --jp num_processes="$processes" --jp num_nodes="$nodes" \
-				  --jp walltime="$walltime" --jp queue=normal --jp launcher=true \
-					  --jp "container=code/containers/bids-${software}" sourcedata/raw \
-						  "$derivatives_path" participant --participant-label '{p[sub]}' \
-							  -w "$work_dir/${raw_ds}_sub-{p[sub]}" -vv "$command"
-	  echo
+		if [ -z "$all_subs_arg" ]; then
+			all_subs=$(find "$raw_path" -maxdepth 1 -type d -name "sub-*" -printf '%f\n' | sed 's/sub-//g' | sort)
+		else
+			all_subs=$(echo "$all_subs_arg" | sed 's/,/\n/g')
+		fi
+
+		# Remove old work dirs
+		for sub in $all_subs; do
+			rm -rf "$work_dir/${raw_ds}_sub-$sub"
+		done
+
+		datalad save -r
+
+		# Submit jobs via reproman in batches 
+		# make sure to 'unlock' outputs
+		echo "$all_subs" | xargs -n "$subs_per_job" echo | while read line; do 
+			sub_list=$(echo "$line" | sed 's/ /,/g')
+			processes=$(echo "$line" | awk '{ print NF }')
+			nodes=$(( ($processes + $subs_per_node - 1) / $subs_per_node)) # round up
+			echo reproman run -r local --sub slurm --orc datalad-no-remote \
+				--bp sub="$sub_list" --input "sourcedata/raw/sub-{p[sub]}" --output . \
+					--jp num_processes="$processes" --jp num_nodes="$nodes" \
+						--jp walltime="$walltime" --jp queue=normal --jp launcher=true \
+							--jp "container=code/containers/bids-${software}" sourcedata/raw \
+								"$derivatives_path" participant --participant-label '{p[sub]}' \
+									-w "$work_dir/${raw_ds}_sub-{p[sub]}" -vv "$command"
+			echo
+		done
 	done
-  done
 }
 
 # initialize variables
@@ -147,7 +146,7 @@ fs_license=$HOME/.freesurfer.txt # this should be in code/license
 # initialize flags
 while [[ "$#" > 1 ]]; do
   case $1 in
-    --no-syn-sdc)
+	--no-syn-sdc)
 		syn_sdc="" ;;
 	--skull-strip-t1w)
 		ss_force=$2; shift ;;
